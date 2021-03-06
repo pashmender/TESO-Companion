57645768231a9afbd2a7c44e3dec770ebdbd42d9
Polekh Pavel <pashmender@gmail.com>
2021-03-07 06:01:59 +0700

    Подключил бутстрап и css файл, контроллер. Разобрался с парой ошибок

---
+++ TESO-Companion/TESO-Companion/Controllers/MainPageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TESO_Companion.Controllers
+{
+    public class MainPageController : Controller
+    {
+        // GET
+        public IActionResult MainPage()
+        {
+            return this.View();
+        }
+    }
+}
--- TESO-Companion/TESO-Companion/Startup.cs
+++ TESO-Companion/TESO-Companion/Startup.cs
@@ -10,12 +10,14 @@ using Microsoft.Extensions.Hosting;
 
 namespace TESO_Companion
 {
-    public class Startup
+    public class  Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,11 +29,12 @@ namespace TESO_Companion
             }
 
             app.UseRouting();
-
+            app.UseStaticFiles();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapControllerRoute("default", "{controller=MainPage}/{action=MainPage}");
             });
+               
         }
     }
 }
