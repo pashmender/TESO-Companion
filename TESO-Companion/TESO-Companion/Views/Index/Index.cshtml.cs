5d9f434ba9a7ebc2a950ef3d1bb01aab92c6538f
Polekh Pavel <pashmender@gmail.com>
2021-05-10 17:58:01 +0700

    Инициализировал страницу и её контроллер

---
+++ TESO-Companion/TESO-Companion/Views/Index/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TESO_Companion.Views
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
