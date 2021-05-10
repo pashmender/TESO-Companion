15b55e333ab674904b3d2017f91d402c57defbdf
Polekh Pavel <pashmender@gmail.com>
2021-05-10 18:02:36 +0700

    Инициализировал страницу журнала и её контроллер

---
+++ TESO-Companion/TESO-Companion/Controllers/JournalPageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TESO_Companion.Controllers
+{
+    public class JournalPageController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
